66478a6e33784dc6523afc636af61a950cc3e452
Max Puig <maxpuig@MacBook-Pro-de-Max.local>
2020-07-27 00:37:06 +0200

    Función para pasar de minutos a texto

---
+++ minsAtxt.js
@@ -0,0 +1,39 @@
+
+function minsAtxt(minutos) {
+    // Añadirá un espacio delante del tiempo. ' 1 día 4 horas'
+    let mins = 0;
+    let horas = 0;
+    let dias = 0;
+    let tiempo = '';
+    if (minutos > 59) {
+        mins = minutos % 60;
+        horas = Math.floor(minutos / 60);
+        if (horas > 23) {
+            dias = Math.floor(horas / 24);
+            horas = horas % 24;
+        }
+    } else {
+        mins = minutos;
+    }
+
+    if (dias == 1) {
+        tiempo += ' 1 día'
+    } else if (dias > 1) {
+        tiempo += ' ' + dias.toString() + ' días'
+    }
+    if (horas == 1) {
+        tiempo += ' 1 hora'
+    } else if (horas > 1) {
+        tiempo += ' ' + horas.toString() + ' horas'
+    } else if (dias != 0 && horas == 0) {
+        tiempo += ' ' + horas.toString() + ' horas'
+    }
+    if (mins == 1) {
+        tiempo += ' 1 minuto'
+    } else if (mins > 1) {
+        tiempo += ' ' + mins.toString() + ' minutos'
+    } else if (dias != 0 || horas != 0 && mins == 0) {
+        tiempo += ' ' + mins.toString() + ' minutos'
+    }
+    return tiempo;
+}
