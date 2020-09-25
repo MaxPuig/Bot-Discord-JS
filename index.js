36246c86975104d9df88f35e5900f2a2e7bf9b95
Max Puig <45133421+MaxPuig@users.noreply.github.com>
2020-09-25 18:47:18 +0200

    Añadido RSS (juegos en oferta)

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const activity = require('./utils/activity.js');
 const borrar = require('./utils/deleteBotEmbeds.js');
 const voicePrank = require('./utils/voicePrank.js');
 const peli = require('./utils/film.js');
-const amongUs = require('./utils/amongUs.js')
+const amongUs = require('./utils/amongUs.js');
+const rss = require('./utils/rss.js');
+const fs = require('fs');
 
 
 
@@ -21,6 +23,13 @@ client.on('ready', async function () {
         await activity.sleep(60000); // cada minuto
         activity.guardarInfo(guildInfo);
         voicePrank.doPrank(i, guildInfo);
+        let Channel = undefined;
+        try {
+            let data = fs.readFileSync('./data/rssChannel.json', 'utf-8');
+            data = JSON.parse(data);
+            Channel = client.channels.cache.get(data);
+        } catch (error) { }
+        rss.sendRSS(i, Channel);
         i++;
     }
 });
@@ -34,12 +43,13 @@ client.on('rateLimit', async function (rateLimitInfo) {
 
 
 client.on('message', async function (msg) {
-    if(msg.content.startsWith('.')){
+    if (msg.content.startsWith('.')) {
         cines.cines(msg);
         ttt.ticTacToe(msg);
         tiempo.tiempoJuegos(msg);
         peli.enviarPeli(msg);
-        amongUs.amongUs(msg)
+        amongUs.amongUs(msg);
+        rss.setRSSchannel(msg);
     };
     borrar.borrarMsg(msg);
     if (msg.content == '.backup') {
