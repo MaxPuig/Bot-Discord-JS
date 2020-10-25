e9bb9c1b2afb72d8064a64b78af9d22758da3051
Max Puig <45133421+MaxPuig@users.noreply.github.com>
2020-10-25 21:08:14 +0100

    Añadido "!p" y "-p". Borrar mensaje ya no da error

--- utils/deleteBotEmbeds.js
+++ utils/deleteBotEmbeds.js
@@ -1,14 +1,14 @@
 require('dotenv').config({path: '../.env'});
 
 
-//Elimina la preview de los links que van dirigidos a los bots de música y sus mensajes
+//Elimina la preview de los links que van dirigidos a los bots de música y también los mensajes de los bot a los 2 mins
 async function borrarMsg(msg){
-    if (msg.content.startsWith('!play ') || msg.content.startsWith('-play ')) {
+    if (msg.content.startsWith('!play ') || msg.content.startsWith('-play ') || msg.content.startsWith('!p ') || msg.content.startsWith('-p ')) {
         msg.suppressEmbeds();
     };
     if (msg.author.id == process.env.BOT_RYTHM || msg.author.id == process.env.BOT_GROOVY) {
         setTimeout(() => {
-            msg.delete();
+            msg.delete().catch(); //.catch() para que no salga un error por consola si alguien borra el mensaje
         }, 120000); //Espera 2 minutos a eliminar los mensajes de los bots
     }
 };
